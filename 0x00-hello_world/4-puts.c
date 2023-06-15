3d6021d53b44a70d8ce16de1d91a260552a8ff7e
mtshwari <tphukubje7@gmail.com>
2023-06-15 08:08:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#!/bin/bash               
+#include <stdio.h>
+/*
+ * main - prints"programmingn is like building a multilingual puzzle
+ * return- 0 if it runs successfuly,otherwise non-zero*/
+
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);}

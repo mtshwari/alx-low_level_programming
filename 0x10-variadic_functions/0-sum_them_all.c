9c593a62fbae6f3e1f06345a6daafdcb7def5d0e
mtshwari <tphukubje7@gmail.com>
2023-07-20 08:05:53 -0700

    fixing loop

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    if (n == 0) {
-        return 0;
-    }
+	
+int sum = 0;
+unsigned int i;
 
-    int sum = 0;
+va_list args;
+va_start(args, n);
 
-    va_list args;
-    va_start(args, n);
+for (i = 0; i < n; ++i) 
+{
 
-    for (unsigned int i = 0; i < n; ++i) {
-        sum += va_arg(args, int);
-    }
+	sum += va_arg(args, int); 
+}
 
-    va_end(args);
+va_end(args);
 
-    return sum;
+return sum;
 }

602dbdfd49cf6a9052a0354381884caa344c1aab
mtshwari <tphukubje7@gmail.com>
2023-07-20 08:10:23 -0700

    betty coding style"

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	
+
 int sum = 0;
 unsigned int i;
 
 va_list args;
 va_start(args, n);
 
-for (i = 0; i < n; ++i) 
+for (i = 0; i < n; ++i)
 {
 
-	sum += va_arg(args, int); 
+	sum += va_arg(args, int);
 }
 
 va_end(args);
 
-return sum;
+return (sum);
 }

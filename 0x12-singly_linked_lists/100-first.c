ec67d97c4a23226ee679854600eb388c6f064c0f
mtshwari <tphukubje7@gmail.com>
2023-07-27 08:59:04 -0700

    initial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void message(void) __attribute__ ((constructor));
+
+/**
+ * message - prints a sentence before the main
+ * function is executed
+ */
+void message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

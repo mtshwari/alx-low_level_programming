bcf6b55fed6dea49cb0934b3c643e36dbd55c5a4
mtshwari <tphukubje7@gmail.com>
2024-03-25 12:17:04 -0700

    generate winning numbers advanced task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
-echo "int rand() { return 9; }" > /tmp/rand.c
-gcc -fPIC -shared -o /tmp/rand.so /tmp/rand.c
-export LD_PRELOAD=/tmp/rand.so
+#!/bin/bash
+echo 'int rand() { static int i = 0; int nums[] = {9, 8, 10, 24, 75, 9}; return nums[i++]; }' > /tmp/custom_rand.c
+gcc -fPIC -shared -o /tmp/custom_rand.so /tmp/custom_rand.c
+export LD_PRELOAD=/tmp/custom_rand.so

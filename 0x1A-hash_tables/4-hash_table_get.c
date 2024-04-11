de7d64efe24963e83053f158d8063c3f74115bfb
mtshwari <tphukubje7@gmail.com>
2024-04-11 12:34:18 -0700

    TASK 4 SEMI COLON ERROR

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,16 +9,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *nodo;
-unsigned long int index;
+	hash_node_t *nodo;
+	unsigned long int index;
 
-if (key == NULL || ht == NULL)
-return (NULL);
+	if (key == NULL || ht == NULL)
+		return (NULL);
 
-index = key_index((const unsigned char *)key, ht->size);
-nodo = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	nodo = ht->array[index];
 
-for (; nodo != NULL; nodo = nodo->next)
-{
-if (strcmp(nodo->key, key) == 0)
-
+	for (; nodo != NULL; nodo = nodo->next)
+	{
+		if (strcmp(nodo->key, key) == 0)
+			return (nodo->value);
+	}
+	return (NULL);
+}
